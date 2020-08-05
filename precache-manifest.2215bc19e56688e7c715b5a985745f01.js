59a98337010161e5ad04771fcf64a0dfebcb0b6f
Mykyta_Prus <mike.prus@ekreative.com>
2020-08-05 08:57:29 +0300

    Updates

---
+++ precache-manifest.2215bc19e56688e7c715b5a985745f01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433d2695da001d8417eb4083dfb4d215",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "284a7fc136ff596da6b2",
+    "url": "/monster-rolodex/static/css/main.4052d140.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "284a7fc136ff596da6b2",
+    "url": "/monster-rolodex/static/js/main.715a8b63.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
